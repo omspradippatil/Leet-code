a6ba020903e5e3f11f1a3f1e0a1dcf7fe0f470e4
om pradip patil <163660845+omspradippatil@users.noreply.github.com>
2026-07-15 22:21:32 +0530

    Time: 2 ms (10.12%), Space: 43.2 MB (6.36%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,5 +1,17 @@
+import java.util.*;
 class Solution {
     public String[] findWords(String[] words) {
-        
-    }
+       ArrayList<String> an=new ArrayList<>();
+       for(String word :words)
+       {
+        if(word.matches("[qwertyuiopQWERTYUIOP]+")||
+        word.matches("[asdfghjklASDFGHJKL]+") ||
+        word.matches("[zxcvbnmZXCVBNM]+"))
+        {
+        an.add(word); 
+        }
+       } 
+    
+    return an.toArray(new String[0]);
+}
 }
