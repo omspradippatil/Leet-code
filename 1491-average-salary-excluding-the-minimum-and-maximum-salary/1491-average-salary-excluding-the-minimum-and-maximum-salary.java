05972a88948cd4ae73e675fa29f7276e3d8ab9e2
om pradip patil <163660845+omspradippatil@users.noreply.github.com>
2026-07-15 22:28:30 +0530

    Time: 0 ms (100%), Space: 45.8 MB (39.15%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double average(int[] salary) {
+        double min=salary[0];
+        double max=salary[0];
+        double sum=0;
+        
+
+        for(int i=0;i<salary.length;i++){
+            if(salary[i]<min){
+                min=salary[i];
+            }
+            if(salary[i]>max){
+                max=salary[i];
+            }
+            sum+=salary[i];
+        }
+         
+           double avgsal = (sum - min - max) / (salary.length - 2);
+
+         return avgsal;
+    }
+    
+    }
